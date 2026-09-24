ac800f89193b07616e3cf7a3bc0ca907dbfc363f
emranhossain-ador <emranhossain.ador@gmail.com>
2026-09-24 07:44:36 +0600

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistOutfit = Outfit({
+  variable: "--font-Outfit",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geistGrotesk = Space_Grotesk({
+  variable: "--font-Space-grotesk",
   subsets: ["latin"],
 });
 
@@ -21,9 +21,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistOutfit.variable} ${geistGrotesk.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        
+        {children}
+
+        </body>
     </html>
   );
 }
